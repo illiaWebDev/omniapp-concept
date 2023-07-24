f35669fe4bfbabc0389fd70f56f95a35aa7b1962
Illia <illiaWebDev@proton.me>
2023-07-24 15:18:27 +0300

    done with improving precommit using semicolons and local var

--- packages/api/src/services/User/adapter/__aggregateTests.ts
+++ packages/api/src/services/User/adapter/__aggregateTests.ts
@@ -2,5 +2,5 @@ import {} from './migrations.test';
 import {} from './create/_.test';
 import {} from './get/_.test';
 import {} from './getAuthParts/_.test';
-// import {} from './getList/_.test';
+import {} from './getList/_.test';
 import {} from './patch/_.test';
